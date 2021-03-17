5839990f6fb2a62f8d82e4715b6970d33c60146c
Asa Holley <asa.holley32@gmail.com>
2021-03-17 19:59:34 -0400

    final R scripts

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,13 @@ summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,
 MechaCar_Suspension<-read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary<-MechaCar_Suspension %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),Standard_deviation_PSI=sd(PSI))
 lot_summary<-MechaCar_Suspension%>% group_by(Manufacturing_Lot)%>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),Standard_deviation_PSI=sd(PSI))
+
+t.test(MechaCar_Suspension$PSI,mu=1500)
+
+Lot1<-subset(MechaCar_Suspension,Manufacturing_Lot=="Lot1")
+Lot2<-subset(MechaCar_Suspension,Manufacturing_Lot=="Lot2")
+Lot3<-subset(MechaCar_Suspension,Manufacturing_Lot=="Lot3")
+
+t.test(Lot1$PSI,mu=1500)
+t.test(Lot2$PSI,mu=1500)
+t.test(Lot3$PSI,mu=1500)
